c946adf59b39b5969e0ce9cf2e337b007a52a35b
wjmao88 <wjmao88@gmail.com>
2014-04-28 15:57:22 -0700

    classify now create new cluster if no cluster is found

--- app/matchmaker/lib/classify.js
+++ app/matchmaker/lib/classify.js
@@ -1,19 +1,43 @@
+var match = function(id, callback){
+  var query = [
+    'MATCH (user:User {userId})-->(:Skill)<--(peer:User)-->(cluster:Cluster)',
+    'RETURN cluster.id, count(peer) ORDER BY count(peer) DESC'
+  ].join(' ');
+
+  var params = {
+    userId: id
+  };
+
+  db.query(query, params, callback);
+}
+
+var create = function(id, callback){
+  var query = [
+    'MATCH (user:User {userId})',
+    'CREATE (cluster:Cluster), (user)-[:BELONGS_TO]->(cluster)',
+    'RETURN cluster.id'
+  ].join(' ');
+
+  var params = {
+    userId: id
+  };
+
+  db.query(query, params, callback);
+}
+
 module.exports = function(db){
   return function(id, callback){
     //find profiles sharing skills with user
     //and examine their clusters
     //pick one to put user in
-    var query = [
-      'MATCH (user:User {userId})-->(:Skill)<--(peer:User)-->(cluster:Cluster)',
-      'RETURN cluster.id, count(peer) ORDER BY count(peer) DESC'
-    ].join(' ');
-
-    var params = {
-      userId: id
-    };
-
-    db.query(query, params, function(results){
-      callback(results[0][0]);
+    match(id, function(results){
+      if (results.length === 0){
+        create(id, function(id){
+          callback(id);
+        })
+      } else {
+        callback(results[0][0]);
+      }
     });
 
     return this;
