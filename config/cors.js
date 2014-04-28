e5b73879c5212214d361f3cc8fa16b471307c0ed
Addison <Addisonedwardlee@gmail.com>
2014-04-28 14:47:40 -0700

    cors working? yes!

--- config/cors.js
+++ config/cors.js
@@ -2,11 +2,11 @@ module.exports =  function(req, res, next){
   res.headers = {
     'access-control-allow-origin': '*',
     'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'access-control-allow-headers': 'content-type, accept',
+    'access-control-allow-headers': '*',
     'access-control-max-age': 10, // Seconds.
     'Content-Type': 'application/json'
   };
-
+  console.log('hit cors!')
   next();
 };
 
