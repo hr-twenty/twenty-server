199f73bc6836156739c0124d625049f3a37135f7
wjmao88 <wjmao88@gmail.com>
2014-04-28 14:04:13 -0700

    add common db connection. add recommendation based on previous choice

--- app/matchmaker/lib/classify.js
+++ app/matchmaker/lib/classify.js
@@ -1,5 +1,8 @@
 module.exports = function(db){
   return function(id, callback){
+    //find profiles sharing skills with user
+    //and examine their clusters
+    //pick one to put user in
     var query = [
       'MATCH (user:User {userId})-->(:Skill)<--(peer:User)-->(cluster:Cluster)',
       'RETURN DISTINCT cluster'
