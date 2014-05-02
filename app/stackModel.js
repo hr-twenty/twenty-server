8bdf489abb4d7add2316ad62470fd5f400690fcd
Addison <Addisonedwardlee@gmail.com>
2014-05-02 12:30:45 -0700

    resolved syntax error on stackModel

--- app/stackModel.js
+++ app/stackModel.js
@@ -37,7 +37,7 @@ exports.getStack = function (data, callback) {
     } else {
     //otherwise, clean up the data and send it out
       processResults(results, callback);
-    // }
+    }
   });
 };
 
