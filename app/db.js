713324979ad3ea53c77091d19ed1381824fd4ece
wjmao88 <wjmao88@gmail.com>
2014-05-07 11:44:54 -0700

    fixed bug with importing env

--- app/db.js
+++ app/db.js
@@ -1,6 +1,5 @@
 var neo4j = require('neo4j');
-var conString = require('../config/env').databaseUri;
-console.log('`````````````` db con', conString);
+var conString = require('../config/env')(process.env.NODE_ENV || 'development').databaseUri;
 var db = new neo4j.GraphDatabase(conString);
 
 module.exports = db;
