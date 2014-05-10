0fc54dbd07f2879b515f2051bcfca6c6b02a2428
wjmao88 <wjmao88@gmail.com>
2014-05-10 16:36:14 -0700

    add exclusion for showing stack

--- app/models/stacks/index.js
+++ app/models/stacks/index.js
@@ -8,6 +8,7 @@ exports.getStack = function (data, callback) {
     'MATCH (user:User {userId:{userId}})-[:HAS_STACK]->(:Stack)-[:STACK_USER]->(other:User)-[:HAS_STACK]->(os:Stack)-[r2]->(user)',
     //Removed this line initially to increase users on stack
     // 'WHERE type(r2) <> "REJECTED"',
+    'WHERE NOT other.id IN {excludeId}'
     'WITH other, r2',
     'LIMIT 20',
     'MATCH (other)-[r3]->(otherInfo)',
@@ -19,7 +20,8 @@ exports.getStack = function (data, callback) {
   ].join('\n');
 
   var params = {
-    userId: data.userId
+    userId: data.userId,
+    excludeId: data.stackIds
   };
 
   // Make sure correct params exist
