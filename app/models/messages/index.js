cdc209dc57e03295264550d80bb20b1a9f14dfd9
Addison <Addisonedwardlee@gmail.com>
2014-05-09 21:08:03 -0700

    fixing missing company from messages v8

--- app/models/messages/index.js
+++ app/models/messages/index.js
@@ -60,7 +60,7 @@ exports.getOne = function(data, callback){
 //Clean up the data from Neo4j before sending to the front end
 var processMessages = function(userId, results, callback){
   var companyHelper = function(company){
-    if(!!company){return [company.data[0]];}
+    if(company.length > 0){return [company.data[0]];}
     else{return 'Not Entered';}
   };
   var finalResults = results.map(function(obj){
